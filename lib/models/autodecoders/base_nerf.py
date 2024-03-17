aaf62ec72e5937611d4b375b669c333c953e0f79
kacperunia <kacpermarzol@gmail.com>
2024-03-17 21:12:18 +0100

    fix print

--- lib/models/autodecoders/base_nerf.py
+++ lib/models/autodecoders/base_nerf.py
@@ -532,11 +532,8 @@ class BaseNeRF(nn.Module):
                 if show_pbar:
                     pbar.update()
             if self.consistency_weight_scheduler is not None:
-                print('ok')
                 self.consistency_weight_scheduler.step()
                 loss_consistency_dict.update(beta=beta)
-            else:
-                print('consistency weight scheduler is none')
 
         decoder.train(decoder_training_prev)
 

60d73a1824c8b9e951c15b974a9407b309a063b8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-11 01:22:42 +0200

    increase decoder size

--- lib/models/autodecoders/diffusion_nerf.py
+++ lib/models/autodecoders/diffusion_nerf.py
@@ -149,7 +149,7 @@ class DiffusionNeRF(MultiSceneNeRF):
             image_multi = clamp_image(image_multi, poses.shape[0])
 
             diff_input = image_multi.reshape(num_scenes, 6, 3, h, w)
-            diff_input = diff_input.view(num_scenes, 3, 6, h, w)
+            diff_input = diff_input.reshape(num_scenes, 3, 6, h, w)
             #diff_input = diff_input.permute(0, 2, 1, 3, 4)
 
         import pickle

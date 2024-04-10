e6a24dbb791aaa1204e3ac9c02c7ec147f6c44e9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-11 00:34:10 +0200

    increase decoder size

--- lib/models/autodecoders/diffusion_nerf.py
+++ lib/models/autodecoders/diffusion_nerf.py
@@ -148,6 +148,9 @@ class DiffusionNeRF(MultiSceneNeRF):
 
             image_multi = clamp_image(image_multi, poses.shape[0])
 
+            print('!!!')
+            print(image_multi.shape)
+
             diff_input = image_multi.reshape(num_scenes, 6, 3, h, w)
             diff_input = diff_input.permute(0, 2, 1, 3, 4)
 

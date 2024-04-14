57c7cd1bb4f960cf251f46fb5d4919dfd96079fc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-14 22:00:37 +0200

    debug

--- configs/paper_cfgs/ssdnerf_mplane_cars_uncond_consistency.py
+++ configs/paper_cfgs/ssdnerf_mplane_cars_uncond_consistency.py
@@ -1,5 +1,5 @@
 import os
-name = 'ssdnerf_mplane_cars_uncond_consistency_regular3'
+name = 'ssdnerf_mplane_cars_uncond_consistency_regular_pl'
 
 DATA_PATH = '/net/tscratch/people/plgmarzol/SSDNeRF_data/shapenet'
 

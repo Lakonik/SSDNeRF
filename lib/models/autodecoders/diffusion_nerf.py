4323d3d92995a4fc2f273329ef63a5399aa8cd89
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-04-16 19:29:10 +0200

    debug

--- lib/models/autodecoders/diffusion_nerf.py
+++ lib/models/autodecoders/diffusion_nerf.py
@@ -165,7 +165,7 @@ class DiffusionNeRF(MultiSceneNeRF):
             if prior_grad is not None:
                 for code_, prior_grad_single in zip(code_list_, prior_grad):
                     code_.grad.copy_(prior_grad_single)
-            loss_decoder.backward()
+            loss_m_decoder.backward()
 
             if 'decoder' in optimizer:
                 optimizer['decoder'].step()
